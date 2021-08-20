f5fa7756b86587fde2ad5a666650126d961b067b
Anthony Sellers <asell0808@yahoo.com>
2021-08-19 23:10:50 -0700

    Update aggregateFunctions.sql

--- aggregateFunctions.sql
+++ aggregateFunctions.sql
@@ -1,8 +1,5 @@
--- to be used with the book_shop db and the books table
-
 -- COUNT will count whatever you want it to count
 SELECT COUNT(*) FROM books;
-
 
 -- COUNT with DISTINCT to show only distinct authors
 SELECT COUNT(DISTINCT(author_fname, author_lname)) FROM books;
@@ -17,7 +14,7 @@ SELECT title, author_lname FROM books GROUP BY author_lname;
 
 -- COUNT how many books each author has written
 SELECT author_fname, author_lname, COUNT(*) FROM books GROUP BY author_lname, author_fname;
-SELECT released_year COUNT(*) FROM books GROUP BY released_year;
+SELECT released_year, COUNT(*) FROM books GROUP BY released_year;
 SELECT CONCAT('In ', released_year, ' ', COUNT(*), ' books released.') AS 'Books released by year' FROM books GROUP BY released_year;
 
 -- MIN and  MAX basics. They identify min and max data from tables
@@ -34,6 +31,27 @@ SELECT MAX(pages) FROM books;
 SELECT * FROM books WHERE pages = (SELECT MAX(pages) FROM books);
 SELECT * FROM books WHERE pages = (SELECT MIN(pages) FROM books);
 -- The problem, running two queries slows down data display, and this would only be worse and worse with more data
--- 
+-- Same result
+SELECT title, pages FROM books ORDER BY pages ASC LIMIT 1;
 
+-- GROUP BY example
+-- Find the year that each author published their first book
+SELECT  author_fname, author_lname, MIN(released_year) FROM books GROUP BY author_fname, author_lname;
 
+-- find longest book by author
+SELECT  author_fname, author_lname, MAX(pages) FROM books GROUP BY author_fname, author_lname;
+
+-- SUM function
+-- SUM of pages of all books
+SELECT SUM(pages) FROM books;
+-- SUM with group by to find the total pages by author
+SELECT author_fname, author_lname, SUM(pages) FROM books GROUP BY author_lname, author_fname;
+
+-- AVG
+-- Calculate average released_year among books
+SELECT AVG(released_year) FROM books;
+SELECT AVG(pages) FROM books;
+
+-- GROUP BY example
+SELECT AVG(stock_quantity) FROM books GROUP BY released_year;
+SELECT author_fname, author_lname, AVG(pages) FROM books GROUP BY author_lname, author_fname;
